22aebd6b065216286087c2f85d4135df2989fdd3
WY <wweiyu@stu.cdu.edu.cn>
2021-12-05 11:58:22 +0800

    测试文件

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author：Weiyu
+ * @Version:
+ * @Since:
+ * @date：2021-12-05_周日 11:57
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
