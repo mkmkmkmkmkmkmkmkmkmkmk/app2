16c17af62670b86a62f1fac8172cfa4c8ab94c02
WY <wweiyu@stu.cdu.edu.cn>
2021-12-05 12:04:03 +0800

    测试文件2

---
+++ src/main/java/Test2.java
@@ -0,0 +1,11 @@
+/**
+ * @Author：Weiyu
+ * @Version:
+ * @Since:
+ * @date：2021-12-05_周日 12:01
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
